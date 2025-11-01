5fd3da6885f9bf2a768729d9c12202fe510831e0
Chuan Ho Danh <chuanhodanh@192.168.1.2>
2025-11-01 22:38:24 +0700

    feat: handle invalid commands

--- app/main.go
+++ app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 )
@@ -11,4 +12,11 @@ var _ = os.Stdout
 
 func main() {
 	fmt.Fprint(os.Stdout, "$ ")
+
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading input: ", err)
+		os.Exit(1)
+	}
+	fmt.Println(command[:len(command)-1] + ": command not found")
 }
