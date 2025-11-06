9997c25884e095a98b2941d4414fa700f0d4016d
Chuan Ho Danh <chuanhodanh@192.168.1.3>
2025-11-06 21:08:26 +0700

    feat: cd command with home directory

--- app/domains/cd_command.go
+++ app/domains/cd_command.go
@@ -13,12 +13,22 @@ func (c *CdCommand) GetName() string {
 
 func (c *CdCommand) Execute(cmd Command) {
 	path := cmd.Args[0]
+	if path == "~" {
+		homeDir, _err := os.UserHomeDir()
+		if _err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to get home directory %s\n", _err.Error())
+			return
+		}
+		path = homeDir
+	}
+
 	_, err := os.Stat(path)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%s: %s: No such file or directory\n", cmd.Name, path)
 		return
 	}
-	err = os.Chdir(cmd.Args[0])
+
+	err = os.Chdir(path)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Failed to change directory to %s\n", cmd.Args[0])
 	}
