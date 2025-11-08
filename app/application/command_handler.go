826cda72fa92f70292082857e25bbd270f3d4c34
Chuan Ho Danh <chuanhodanh@192.168.1.2>
2025-11-08 22:43:31 +0700

    feat: single quotes

--- app/application/command_handler.go
+++ app/application/command_handler.go
@@ -29,7 +29,5 @@ func (ch *CommandHandler) HandleCommand() {
 		}
 
 		ch.registry.Execute(*cmd)
-
-		// reader.Reset(os.Stdin)
 	}
 }
--- app/application/command_parser.go
+++ app/application/command_parser.go
@@ -17,6 +17,53 @@ func NewCommandParser(reader *bufio.Reader) *CommandParser {
 	return &CommandParser{reader: reader}
 }
 
+type State int
+
+const (
+	Unquoted State = iota
+	SingleQuoted
+)
+
+func tokenize(s string) ([]string, error) {
+	state := Unquoted
+	var tok []string
+	var buf []rune
+
+	flush := func() {
+		if len(buf) > 0 {
+			tok = append(tok, string(buf))
+			buf = nil
+		}
+	}
+
+	singleQuote := '\''
+	for _, c := range s {
+		switch state {
+		case Unquoted:
+			switch c {
+			case ' ', '\t':
+				flush()
+			case singleQuote:
+				state = SingleQuoted
+			default:
+				buf = append(buf, c)
+			}
+		case SingleQuoted:
+			if c == singleQuote {
+				state = Unquoted
+			} else {
+				buf = append(buf, c)
+			}
+		}
+	}
+	if state == SingleQuoted {
+		return nil, fmt.Errorf("Unmached '")
+	}
+	flush()
+
+	return tok, nil
+}
+
 func (parser *CommandParser) ParseCommand() (*domains.Command, error) {
 	content, err := parser.reader.ReadString('\n')
 	if err != nil {
@@ -24,11 +71,16 @@ func (parser *CommandParser) ParseCommand() (*domains.Command, error) {
 		return nil, err
 	}
 
-	// Split read content to command and its arguments
-	elems := strings.Fields(content)
+	cmd, argumentStr, _ := strings.Cut(strings.TrimSpace(content), " ")
+	args, err := tokenize(argumentStr)
+
+	if err != nil {
+		fmt.Fprint(os.Stderr, "ERROR: "+err.Error())
+		return nil, err
+	}
 
 	return &domains.Command{
-		Name: elems[0],
-		Args: elems[1:],
+		Name: cmd,
+		Args: args,
 	}, nil
 }
