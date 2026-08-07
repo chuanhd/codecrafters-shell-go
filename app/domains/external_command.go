3848dfdd9b988a412996ff83419f0423ce9d0f2d
chuan_hd <chuanhodanh@192.168.1.6>
2026-08-07 17:13:26 +0700

    codecrafters submit [skip ci]

--- app/domains/external_command.go
+++ app/domains/external_command.go
@@ -1,6 +1,8 @@
 package domains
 
 import (
+	"fmt"
+	"os"
 	"os/exec"
 )
 
@@ -11,10 +13,23 @@ func (c *ExternalCommand) GetName() string {
 }
 
 func (c *ExternalCommand) Execute(cmd *Command) error {
-	externalCmd := exec.Command(cmd.Name, cmd.Args...)
-	externalCmd.Stdout = cmd.Writer
-	externalCmd.Stderr = cmd.ErrWriter
-	externalCmd.Stdin = cmd.Stdin
+	lastArg := cmd.Args[len(cmd.Args)-1]
+	// If lastArg is &, this job will run in background
+	if lastArg == "&" {
+		args := cmd.Args[:len(cmd.Args)]
+		externalCmd := exec.Command(cmd.Name, args...)
+		err := externalCmd.Start()
+		if err != nil {
+			return err
+		}
+		fmt.Fprintf(os.Stdout, "[1] %d\n", cmd.Name, externalCmd.Process.Pid)
+		return nil
+	} else {
+		externalCmd := exec.Command(cmd.Name, cmd.Args...)
+		externalCmd.Stdout = cmd.Writer
+		externalCmd.Stderr = cmd.ErrWriter
+		externalCmd.Stdin = cmd.Stdin
 
-	return externalCmd.Run()
+		return externalCmd.Run()
+	}
 }
